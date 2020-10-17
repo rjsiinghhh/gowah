572e05c96b6b43edd4784959cde9394af94b9456
AlexV77 <53448365+AlexV77@users.noreply.github.com>
2020-10-17 14:21:07 -0400

    react

--- app.js
+++ app.js
@@ -1,56 +1,123 @@
 class App extends React.Component {
     state = {
-        bids: []
+        homes: []
     }
     componentDidMount = () => {
-        this.updaterealEstate();
+        this.updateHome();
     }
-    changeUpdateBidsListing = (event) => {
+    changeUpdateHomesPrice = (event) => {
         this.setState({
-            updateBidsListing:event.target.value
+            updateHomesPrice:event.target.value
         })
     }
-    changeUpdateHomePrice = (event) => {
+    changeUpdateHomesBedrooms = (event) => {
         this.setState({
-            updateBidsPrice:event.target.value
+            updateHomesBedroom:event.target.value
         })
     }
-    deletePerson = (event) => {
+    changeUpdateHomesBathrooms = (event) => {
+        this.setState({
+            updateHomesBathrooms:event.target.value
+        })
+    }
+    changeUpdateHomesSquareft = (event) => {
+        this.setState({
+            updateHomesSquareft:event.target.value
+        })
+    }
+    changeUpdateHomesHouseNumber = (event) => {
+        this.setState({
+            updateHomesHouseNumber:event.target.value
+        })
+    }
+    changeUpdateHomesStreetName = (event) => {
+        this.setState({
+            updateHomesStreetName:event.target.value
+        })
+    }
+    changeUpdateHomesCity = (event) => {
+        this.setState({
+            updateHomesCity:event.target.value
+        })
+    }
+    changeUpdateHomesState = (event) => {
+        this.setState({
+            updateHomesState:event.target.value
+        })
+    }
+    changeUpdateHomesZip = (event) => {
+        this.setState({
+            updateHomesZip:event.target.value
+        })
+    }
+    changeUpdateHomesImage_Link = (event) => {
+        this.setState({
+            updateHomesImage_Link:event.target.value
+        })
+    }
+    changeUpdateHomesBid_Price = (event) => {
+        this.setState({
+            updateHomesBid_Price:event.target.value
+        })
+    }
+    changeUpdateHomesCallBack_Phone = (event) => {
+        this.setState({
+            updateHomesCallBack_Phone:event.target.value
+        })
+    }
+    changeUpdateHomesSet_Date = (event) => {
+        this.setState({
+            updateHomesSet_Date:event.target.value
+        })
+    }
+    deleteHomes = (event) => {
         axios.delete('/realestate/' + event.target.value).then(
             (response) => {
                 this.setState(
                     {
-                        realEstate:response.data
+                        home:response.data
                     }
                 )
             }
         )
     }
-    updateBids = (event) => {
+    updateHomes = (event) => {
         event.preventDefault();
         const id = event.target.getAttribute('id');
         axios.put(
             '/realestate/'+id,
             {
-                listing:this.state.updateBidsListing,
-                pricew:this.state.updateBidsPrice
+                price:this.state.updateHomesPrice,
+                bedrooms:this.state.updateHomesBedrooms,
+                bathrooms:this.state.updateHomesBathrooms,
+                squareft:this.state.updateHomesSquareft,
+                housenumber:this.state.updateHomesHouseNumber,
+                streetname:this.state.updateHomesStreetName,
+                city:this.state.updateHomesCity,
+                state:this.state.updateHomesState,
+                zip:this.state.updateHomesZip,
+                image_link:this.state.updateHomesImage_Link,
+                bid_price:this.state.updateHomesBid_Price,
+                callback_phone:this.state.updateHomesCallBack_Phone,
+                set_date:this.state.updateHomesSet_Date,
+                    
             }
         ).then(
             (response) => {
                 this.setState(
                     {
-                        realEstate:response.data
+                        home:response.data
                     }
                 )
             }
         )
     }
-    updateBids = () => {
+    updateHomes = () => {
         axios.get('/realestate').then(
             (response) => {
                 this.setState(
                     {
-                        bids:response.data
+                        home:response.data
                     }
                 )
             }
@@ -58,21 +125,32 @@ class App extends React.Component {
     }
     render = () => {
         return <div>
-            <CreateForm createCallback={this.updaterealEstate}></CreateForm>
+            <CreateForm createCallback={this.updateHome}></CreateForm>
             <h2>Houses for Sale!</h2>
             <ul>
                 {
-                    this.state.realEstate.map(
-                        () => {
+                    this.state.home.map(
+                        (homes) => {
                             return <li>
-                                {bids.price}: {bids.listing}
-                                <button value={bids.id} onClick={this.deleteBids}>
+                                {homes.price}: {homes.bedrooms}: {homes.bathrooms}: {homes.squareft}: {homes.housenumber}: {homes.streetname}: {homes.city}: {homes.state}: {homes.zip}: {homes.image_link}: {homes.bid_price}: {homes.callback_phone}: {homes.set_date}
+                                <button value={homes.id} onClick={this.deleteHomes}>
                                     Delete
                                 </button>
-                                <form id={bids.id} onSubmit={this.updateBids}>
-                                    <input onKeyUp={this.changeUpdateBidsPrice} type="number" placeholder="price" /><br/>
-                                    <input onKeyUp={this.changeUpdateBidsListing} type="text" placeholder="listing" /><br/>
-                                    <input type="submit" value="Update Bids" />
+                                <form id={homes.id} onSubmit={this.updateHomes}>
+                                    <input onKeyUp={this.changeUpdateHomesPrice} type="number" placeholder="price" /><br/>
+                                    <input onKeyUp={this.changeUpdateHomesBedrooms} type="number" placeholder="bedrooms" /><br/>
+                                    <input onKeyUp={this.changeUpdateHomesBathrooms} type="number" placeholder="bathrooms" /><br/>
+                                    <input onKeyUp={this.changeUpdateHomesSquareft} type="number" placeholder="squareft" /><br/>
+                                    <input onKeyUp={this.changeUpdateHomesHouseNumber} type="number" placeholder="housenumber" /><br/>
+                                    <input onKeyUp={this.changeUpdateHomesStreetName} type="text" placeholder="streetname" /><br/>
+                                    <input onKeyUp={this.changeUpdateHomesCity} type="text" placeholder="city" /><br/>
+                                    <input onKeyUp={this.changeUpdateHomesState} type="text" placeholder="state" /><br/>
+                                    <input onKeyUp={this.changeUpdateHomesZip} type="number" placeholder="zip" /><br/>
+                                    <input onKeyUp={this.changeUpdateHomesImage_Link} type="image" placeholder="imagelink" /><br/>
+                                    <input onKeyUp={this.changeUpdateHomesBid_Price} type="number" placeholder="bidprice" /><br/>
+                                    <input onKeyUp={this.changeUpdateHomesCallBack_Phone} type="number" placeholder="callbackphone" /><br/>
+                                    <input onKeyUp={this.changeUpdateHomesSet_Date} type="number" placeholder="setdate" /><br/>
+                                    <input type="submit" value="Update Homes" />
                                 </form>
                             </li>
                         }
--- createform.js
+++ createform.js
@@ -1,11 +1,22 @@
 class CreateForm extends React.Component {
-    createBids = (event) => {
+    createHomes = (event) => {
         event.preventDefault();
         axios.post(
             '/realestate',
             {
-                price:this.state.newBidsPrice,
-                listing:this.state.newBidsListing
+                price:this.state.updateHomesPrice,
+                bedrooms:this.state.updateHomesBedrooms,
+                bathrooms:this.state.updateHomesBathrooms,
+                squareft:this.state.updateHomesSquareft,
+                housenumber:this.state.updateHomesHouseNumber,
+                streetname:this.state.updateHomesStreetName,
+                city:this.state.updateHomesCity,
+                state:this.state.updateHomesState,
+                zip:this.state.updateHomesZip,
+                image_link:this.state.updateHomesImage_Link,
+                bid_price:this.state.updateHomesBid_Price,
+                callback_phone:this.state.updateHomesCallBack_Phone,
+                set_date:this.state.updateHomesSet_Date,
             }
         ).then(
             (response) => {
@@ -13,23 +24,89 @@ class CreateForm extends React.Component {
             }
         )
     }
-    changeNewBidsListing = (event) => {
+    changeNewHomesPrice = (event) => {
         this.setState({
-            newBidsListing:event.target.value
+            newHomesPrice:event.target.value
         })
     }
-    changeNewBidsPrice = (event) => {
+    changeNewHomesBedrooms = (event) => {
         this.setState({
-            newBidsPrice:event.target.value
+            newHomesBedroom:event.target.value
+        })
+    }
+    changeNewHomesBathrooms = (event) => {
+        this.setState({
+            newHomesBathrooms:event.target.value
+        })
+    }
+    changeNewHomesSquareft = (event) => {
+        this.setState({
+            newHomesSquareft:event.target.value
+        })
+    }
+    changeNewHomesHouseNumber = (event) => {
+        this.setState({
+            newHomesHouseNumber:event.target.value
+        })
+    }
+    changeNewHomesStreetName = (event) => {
+        this.setState({
+            newHomesStreetName:event.target.value
+        })
+    }
+    changeNewHomesCity = (event) => {
+        this.setState({
+            newHomesCity:event.target.value
+        })
+    }
+    changeNewHomesState = (event) => {
+        this.setState({
+            newHomesState:event.target.value
+        })
+    }
+    changeNewHomesZip = (event) => {
+        this.setState({
+            newHomesZip:event.target.value
+        })
+    }
+    changeNewHomesImage_Link = (event) => {
+        this.setState({
+            newHomesImage_Link:event.target.value
+        })
+    }
+    changeNewHomesBid_Price = (event) => {
+        this.setState({
+            newHomesBid_Price:event.target.value
+        })
+    }
+    changeNewHomesCallBack_Phone = (event) => {
+        this.setState({
+            newHomesCallBack_Phone:event.target.value
+        })
+    }
+    changeNewHomesSet_Date = (event) => {
+        this.setState({
+            newHomesSet_Date:event.target.value
         })
     }
     render = () => {
         return <div>
-            <h2>Create Bids</h2>
-            <form onSubmit={this.createBids}>
-                <input onKeyUp={this.changeNewBidsPrice} type="number" placeholder="price" /><br/>
-                <input onKeyUp={this.changeNewBidsListing} type="text" placeholder="listing" /><br/>
-                <input type="submit" value="Create BIds"/>
+            <h2>Create Homes</h2>
+            <form onSubmit={this.createHomes}>
+            <input onKeyUp={this.changeNewHomesPrice} type="number" placeholder="price" /><br/>
+                                    <input onKeyUp={this.changeNewHomesBedrooms} type="number" placeholder="bedrooms" /><br/>
+                                    <input onKeyUp={this.changeNewHomesBathrooms} type="number" placeholder="bathrooms" /><br/>
+                                    <input onKeyUp={this.changeNewHomesSquareft} type="number" placeholder="squareft" /><br/>
+                                    <input onKeyUp={this.changeNewHomesHouseNumber} type="number" placeholder="housenumber" /><br/>
+                                    <input onKeyUp={this.changeNewHomesStreetName} type="text" placeholder="streetname" /><br/>
+                                    <input onKeyUp={this.changeNewHomesCity} type="text" placeholder="city" /><br/>
+                                    <input onKeyUp={this.changeNewHomesState} type="text" placeholder="state" /><br/>
+                                    <input onKeyUp={this.changeNewHomesZip} type="number" placeholder="zip" /><br/>
+                                    <input onKeyUp={this.changeNewHomesImage_Link} type="image" placeholder="imagelink" /><br/>
+                                    <input onKeyUp={this.changeNewHomesBid_Price} type="number" placeholder="bidprice" /><br/>
+                                    <input onKeyUp={this.changeNewHomesCallBack_Phone} type="number" placeholder="callbackphone" /><br/>
+                                    <input onKeyUp={this.changeNewHomesSet_Date} type="number" placeholder="setdate" /><br/>
+                <input type="submit" value="Create Homes"/>
             </form>
         </div>
     }
