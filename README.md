0aad0975b702ac4b3f97495906de9536e478a83d
Arjun Singh <xsinghxarjun@gmail.com>
2020-10-18 11:39:34 -0700

    rj

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # projectfour
+
+
+Gowąh which means home is an app similar to zillow which shows listings and gives users to the chance to make an offer on the properties. All of which are for sale, and specific to the San Francisco Area.
--- app.js
+++ app.js
@@ -1,76 +1,122 @@
-class App extends React.Component{
-  state = {
-    homes : []
-  }
+class App extends React.Component {
+    state = {
+        homes: []
+    }
 
-  componentDidMount = () => {
-    axios.get('/homes').then(
-      (response) => {
-        this.setState(
-          {
-            homes: response.data
-          }
+    componentDidMount = () => {
+        this.updateHomes();
+    }
+
+    changeUpdateHomeName = (event) => {
+        this.setState({
+            updateHomeBid_price:event.target.value
+        })
+    }
+
+    changeUpdateHomeCallback_phone = (event) => {
+        this.setState({
+            updateHomeCallback_phone:event.target.value
+        })
+    }
+    changeUpdateHomeSet_date = (event) => {
+      this.setState({
+        changeUpdateHomeSet_date: event.target.value
+      })
+    }
+
+    deleteHome = (event) => {
+        axios.delete('/homes/' + event.target.value).then(
+            (response) => {
+                this.setState(
+                    {
+                        homes:response.data
+                    }
+                )
+            }
         )
-      }
-    )
-  }
-
-deleteHome = (event) => {
-  axios.delete('/homes/') + event.target.value).then(
-    (response) =>{
-      this.setState(
-        {
-          homes: response.data
-        }
-      )
     }
-  )
-}
 
-updateHomes = (allPeople) => {
-  this.setState(
-    {
-      homes: allHomes
+    updateHome = (event) => {
+        event.preventDefault();
+        const id = event.target.getAttribute('id');
+        axios.put(
+            '/homes/'+id,
+            {
+                bid_price:this.state.updateHomeBid_price,
+                callback_phone:this.state.updateHomeCallback_phone,
+                set_date: this.state.updateHomeSet_date
+            }
+        ).then(
+            (response) => {
+                this.setState(
+                    {
+                        homes:response.data
+                    }
+                )
+            }
+        )
     }
-  )
+
+    updateHomes = () => {
+        axios.get('/homes').then(
+            (response) => {
+                this.setState(
+                    {
+                        homes:response.data
+                    }
+                )
+            }
+        )
+    }
+
+    render = () => {
+        return <div>
+            <h2>Listings</h2>
+            <ul>
+                {
+                    this.state.homes.map(
+                        (home) => {
+                            return <div className="container">
+                            <li>
+                             <img className="pics" src={home.image_link} alt="home-pics"/><br/>
+                                Asking ${home.price}<br/>
+                                {home.bedrooms}<br/>
+                                {home.bathrooms}<br/>
+                                {home.sqaureft}<br/>
+                                Address:{home.housenumber}
+                                {home.streetname}
+                                {home.city}<br/>
+                                 {home.state}<br/>
+                                 {home.zip}<br/>
+
+                                 Highest Bid: {home.bid_price}
+                                 <br/> {home.callback_phone}
+
+                                <form id={home.id} onSubmit={this.updateHome}>
+                                    <input onKeyUp={this.changeUpdateHomeBid_price} type="text" placeholder="Make An Offer?" /><br/>
+                                    <input onKeyUp={this.changeUpdateHomeCallback_phone} type="text" placeholder="Leave Your Phone Number" /><br/>
+                                    <input onKeyUp={this.changeUpdateHomeSet_date} type="text" placeholder="Set An Appointment" /> <br />
+                                    <input type="submit" value="Update Bid" />
+                                </form>
+                                <button value={home.id} onClick={this.deleteHome}>
+                                    Delete
+                                </button>
+                            </li>
+                            </div>
+                        }
+                    )
+                }
+            </ul>
+        </div>
+    }
 }
 
-
-}
-
-
-render = () => {
-  return <div>
-  <CreateForm createCallback= {this.updateHomes}> </CreateForm>
-  <h2> Listings</h2>
-  <ul>
-  {
-    this.state.homes.map(
-      (home) => {
-        return <li>
-        {home.bid_price}: {home.callback_phone}
-        <button value={home.id} onClick={this.deleteHome}>
-        Delete
-        </button>
-        <UpdateForm
-        updateCallback={this.updateHomes}
-        home={home}>
-        </UpdateForm>
-        </li>
-      }
-    )
-  }
-
-  <ul>
-  </div>
-
-
-
-}
-
-
+// const App = (props) => {
+//     return <h2>Functional {props.name}!</h2>
+// }
 
 ReactDOM.render(
-  <App></App>,
-  document.querySelector('main')
+    <App></App>,
+    document.querySelector('main')
+
 )
