e07bb662334eccb813ad65c84851fabb5dcc0206
Molly Haughey <mollyhaughey@Mollys-MBP.cable.rcn.com>
2020-10-19 01:31:15 -0400

    styling

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { Collapsible, CollapsibleItem } from 'react-materialize';
+
 class App extends React.Component {
     state = {
         homes: []
@@ -71,51 +73,66 @@ class App extends React.Component {
 
     render = () => {
         return <div>
-            <div className="header-container">
-            <div className="header">
-            <img src="https://i.imgur.com/kU1OKYf.png"/>
-            </div>
+        <div className="header-container">
+                <div className="header">
+                    <img src="https://i.imgur.com/K106xP8.png"/>
+                </div>
             </div>
             <h2>Listings</h2>
-            <div className="big-container"/>
-            <ul>
-                {
-                    this.state.homes.map(
-                        (home) => {
-                            return <div className="container">
-                                <li>                           
-                             <img className="pics" src={home.image_link} alt="home-pics"/><br/>
-                                ${home.price}<br/>
-                                {home.bedrooms}<br/>
-                                {home.bathrooms}<br/>
-                                {home.sqaureft}<br/>
-                                {home.housenumber}
-                                {home.streetname}
-                                {home.city}<br/>
-                                 {home.state}<br/>
-                                 {home.zip}<br/>
+            <div className="big">
+                <ul>
+                    {
+                        this.state.homes.map(
+                            (home) => {
+                                return <div className="container">
+                                    <li>                           
+                                        <img className="card-panel hoverable" src={home.image_link} alt="home-pics" width="400"/><br/>
+                                        <Collapsible accordion={false}>
+                                        <CollapsibleItem 
+                                        expanded={false} 
+                                        header="View More"
+                                        node="div"
+                                        >
+                                        ${home.price}<br/>
+                                        {home.bedrooms}<br/>
+                                        {home.bathrooms}<br/>
+                                        {home.sqaureft}<br/>
+                                        {home.housenumber}
+                                        {home.streetname}
+                                        {home.city}<br/>
+                                        {home.state}<br/>
+                                        {home.zip}<br/>
 
-                                 <br/>{home.bid_price}
-                                 <br/> {home.callback_phone}
+                                        <br/>{home.bid_price}
+                                        <br/> {home.callback_phone}
+                                        </CollapsibleItem>
+                                        </Collapsible>
+                                       
 
-                                <form id={home.id} onSubmit={this.updateHome}>
-                                    <input onKeyUp={this.changeUpdateHomeBid_price} type="text" placeholder="Make An Offer?" /><br/>
-                                    <input onKeyUp={this.changeUpdateHomeCallback_phone} type="text" placeholder="Leave Your Phone Number" /><br/>
-                                    <input onKeyUp={this.changeUpdateHomeSet_date} type="text" placeholder="Set An Appointment" /> <br />
-                                    <input type="submit" value="Update Bid" />
-                                </form>
-                                <button value={home.id} onClick={this.deleteHome}>
-                                    Delete
-                                </button>                            
-                                </li>
-                            </div>
-                        }
-                    )
-                }
-            </ul>
-        </div>
-    }
+                                        <form id={home.id} onSubmit={this.updateHome}>
+                                        <input onKeyUp={this.changeUpdateHomeBid_price} type="text" placeholder="Make An Offer?" /><br/>
+                                        <input onKeyUp={this.changeUpdateHomeCallback_phone} type="text" placeholder="Leave Your Phone Number" /><br/>
+                                        <input onKeyUp={this.changeUpdateHomeSet_date} type="text" placeholder="Set An Appointment" /> <br />
+                                        <input className="waves-effect waves-light btn-small" type="submit" value="Update Bid" />
+                                        </form><br/>
+                                        <button className="waves-effect waves-light btn-small" value={home.id} onClick={this.deleteHome}>
+                                            Delete
+                                        </button>                            
+                                    </li>
+                                </div>
+                                
+                            }
+                        )
+                        
+                    }
+                </ul>
+            </div>
+            </div>
+        }
+           
 }
+
+
 
 // const App = (props) => {
 //     return <h2>Functional {props.name}!</h2>
